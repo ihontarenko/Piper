7ac7294f9480eb2547f73e6cbd41f72c04fdbdea
Ivan Hontarenko <ivan_hontarenko@epam.com>
2022-08-09 10:00:32 +0300

    RC01

--- src/main/java/pro/javadev/piper/execution/build/LinuxProcessBuilder.java
+++ src/main/java/pro/javadev/piper/execution/build/LinuxProcessBuilder.java
@@ -3,7 +3,7 @@ package pro.javadev.piper.execution.build;
 public class LinuxProcessBuilder extends AbstractProcessBuilder {
 
     public LinuxProcessBuilder() {
-
+        super("/bin/bash", "-c");
     }
 
 }
